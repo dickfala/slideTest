be370bf030ac58508eaf7c89a4b6ebd0beec2e3a
james <dickfala@gmail.com>
2015-02-08 14:22:27 +0800

    Initial Commit

---
+++ SlideAutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideAutoLayoutDemo
+//
+//  Created by Chang YuanYu on 2015/2/8.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideAutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideAutoLayoutDemo
+//
+//  Created by Chang YuanYu on 2015/2/8.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
